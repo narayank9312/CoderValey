7f00d29dbcc56315bc7c7d87dcbf8d63de876467
Narayan Kumar Jha <narayank9312@gmail.com>
2021-03-01 16:49:22 +0530

    navbar
    Signed-off-by: Narayan Kumar Jha <narayank9312@gmail.com>

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import web from '../src/img/rocket.jpg';
 
 function Home() {
-    return (
-        <div>
-            <h1> Welcome Home Page </h1>
+  return (
+    <>
+      <section id="header" className=" d-flex align-items-centre">
+        <div className="container-fluid nav_bg">
+          <div className="row">
+            <div className="col-10 mx-auto">
+              <div className="row">
+                <div classname="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-centre flex-column">
+                  <h1>
+                    Grow your skill with
+                    <br />
+                    <strong className="brand-name"> CodersValey </strong>
+                  </h1>
+                  <h2 className="my-3">
+                    We are the team of talented developer solving your doubts
+                  </h2>
+                  <div className="mt-3">
+                    <NavLink to="/service" className="btn-get-started">
+                      Get Started
+                    </NavLink>
+                  </div>
+                </div>
+
+                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                  <img
+                    src={web}
+                    className="img-fluid animated"
+                    alt=" hom img "
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/service.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Service() {
+    return (
+        <div>
+            <h1> Welcome service Page </h1>
+        </div>
+    )
+}
+
+export default Service
